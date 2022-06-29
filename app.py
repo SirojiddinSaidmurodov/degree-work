295bb1dfd7b9aa28782a3238e47db7ef1a534837
Sirojiddin Saidmurodov <siroj.serj15@outlook.com>
2022-06-29 16:14:52 +0300

    update app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import gradio as gr
 
+from predictor import prepare_hyperparameters, model_and_tokenizer_initialize, inference
 
-def greet(name):
-    return name + ' лох'
+hyperparameters = prepare_hyperparameters()
+BATCH_SIZE = 64
+
+puncRec, tokenizer = model_and_tokenizer_initialize(hyperparameters)
+
+
+def predict(text):
+    return inference(text, puncRec, tokenizer, hyperparameters, BATCH_SIZE)
 
 
 title = " PuncRec - Punctuation and capitalization recovery for tatar language"
@@ -18,10 +25,17 @@ textbox = gr.Textbox(
     placeholder="Text",
     lines=2,
 )
-examples = [["What are you doing?"], ["Where should we time travel to?"]]
+examples = [["Татарстан бүгенге көндә – Россиянең үзенчәлекле һәм мөстәкыйль төбәге. Без яңа "
+             "эш алымнарын һәм заманча технологияләрне, беренче чиратта, нефть чыгару, "
+             "нефть эшкәртү һәм нефть химиясе, машина төзү тармакларында һәм IТ-өлкәдә "
+             "кулланышка кертү буенча алдынгы урынны биләп торабыз. Татарстанда гаять куәтле "
+             "производстволар ачылды һәм алар уңышлы эшләп килә. Ил картасында яңа "
+             "Иннополис шәһәре пәйда булды. Казан Россиянең спорт башкаласы дигән исемгә "
+             "хаклы рәвештә лаек булуын раслады, тарихи-мәдәни мирасны торгызу өлкәсендә дә "
+             "җитди адымнар ясалды."]]
 
 # нәтиҗә
-demo = gr.Interface(fn=greet,
+demo = gr.Interface(fn=predict,
                     inputs=textbox,
                     outputs="text",
                     title=title,
@@ -31,4 +45,4 @@ demo = gr.Interface(fn=greet,
                     )
 
 if __name__ == '__main__':
-    demo.launch(share=True)
+    demo.launch()
